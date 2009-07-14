92fe31c2b91a96c77a95fe1a59af72138402d9e6
Peat Bakke <peat@peat.org>
2009-07-14 01:17:17 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,19 @@
+// $Id$
+
+Module: RPX
+Author: Nathan Rambeck <www.rambeck.com>
+
+
+Description
+===========
+
+
+
+Installation
+============
+1. Enable the module in Drupal
+2. Register/Configure the RPX API Key at Administer >> Site Configuration >> RPX Sign In
+
+
+Usage
+=====
---
+++ rpx.class.php
@@ -0,0 +1,112 @@
+<?
+/*
+  Generic RPX Library
+*/
+
+define('RPX_LIBRARY_VERSION', '0.1');
+
+class RPX {
+
+  const create_api = 'https://rpxnow.com/plugin/create_rp';
+  const lookup_api = 'https://rpxnow.com/plugin/lookup_rp';
+  const auth_api = 'https://rpxnow.com/api/v2/auth_info';
+    
+  /* performs a lookup request for getting information about an RPX account */
+  function lookup( $value, $type ) {
+    $post_data = array(
+        $type => $value,
+        'demographics' => 'PHP RPX' . RPX_LIBRARY_VERSION
+      );
+    
+    $raw_result = RPX::http_post( RPX::lookup_api, $post_data );
+    return json_decode( $raw_result, true );
+  }
+  
+  /* fetches authorization information from a token */
+  function auth_info( $token, $api_key ) {
+    $post_data = array(
+        'token' => $token,
+        'apiKey' => $api_key,
+        'format' => 'json'
+      );
+      
+      $raw_result = RPX::http_post( RPX::auth_api, $post_data );
+      return json_decode( $raw_result, true );
+  }
+
+  function locales() { 
+    return array('pt-BR', 'vi', 'zh', 'nl', 'sr', 'ko', 'ru', 'sv-SE', 'ro', 'pt', 'it', 'hu', 'fr', 'ja', 'en', 'bg', 'es', 'el', 'pl', 'de', 'cs', 'da');
+  }
+  
+  /*
+    Everything below is an internal utility function
+  */
+
+
+  /* builds the current URL for the page being looked at */
+  function current_url() {
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on') {                    
+      $proto = "https";
+      $standard_port = '443';                                                 
+    } else {                                                                    
+      $proto = 'http';                                                        
+      $standard_port = '80';                                                  
+    }                                                                           
+
+    $authority = $_SERVER['HTTP_HOST'];                                         
+    if (strpos($authority, ':') === FALSE &&                                    
+        $_SERVER['SERVER_PORT'] != $standard_port) {                            
+      $authority .= ':' . $_SERVER['SERVER_PORT'];                            
+    }                                                                           
+
+    if (isset($_SERVER['REQUEST_URI'])) {                                       
+      $request_uri = $_SERVER['REQUEST_URI'];                                 
+    } else {                                                                    
+      $request_uri = $_SERVER['SCRIPT_NAME'] . $_SERVER['PATH_INFO'];         
+      $query = $_SERVER['QUERY_STRING'];                                      
+      if (isset($query)) {                                                    
+        $request_uri .= '?' . $query;                                       
+      }                                                                       
+    }                                                                           
+
+    return $proto . '://' . $authority . $request_uri;                          
+  }
+
+
+  /* next three functions are for sending HTTP POST requests */
+  function http_post($url, $post_data) {
+    if (function_exists('curl_init')) {
+      return RPX::curl_http_post($url, $post_data);
+    } else {
+      return RPX::builtin_http_post($url, $post_data);
+    }
+  }
+
+  function curl_http_post($url, $post_data) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $raw_data = curl_exec($curl);
+    curl_close($curl);
+    return $raw_data;
+  }
+
+
+  function rpx_builtin_http_post($url, $post_data) {
+    $content = http_build_query($post_data);
+    $opts = array('http'=>array('method'=>"POST", 'content'=>$content));
+    $context = stream_context_create($opts);
+    $raw_data = file_get_contents($url, 0, $context);
+    return $raw_data;
+  }
+
+
+
+}
+
+
+?>
